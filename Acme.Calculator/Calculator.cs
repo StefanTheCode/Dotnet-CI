e4a791ae64ce43b5c64a568c0dfa27426754f6f9
StefanTheCode <stefan.djokic1995@gmail.com>
2025-11-09 15:26:42 +0100

    Added Calculator Logic

---
+++ Acme.Calculator/Calculator.cs
@@ -0,0 +1,10 @@
+using System; // unused
+using System.Threading.Tasks; // unused
+
+namespace Acme.Calculator
+{
+    public class Calculator
+    {
+        public int Add(int a,int b) {  return a + b; } // bad spacing + analyzer warnings
+    }
+}

fb9e83035d2b1bb8239ddc4189b7bd62b847b766
StefanTheCode <stefan.djokic1995@gmail.com>
2025-11-09 15:02:54 +0100

    Initial configuration

---
+++ Acme/Acme.Calculator/Calculator.cs
@@ -0,0 +1,10 @@
+﻿using System; // unused
+using System.Threading.Tasks; // unused
+
+namespace Acme.Calculator
+{
+    public class Calculator
+    {
+        public int Add(int a,int b) {  return a + b; } // bad spacing + analyzer warnings
+    }
+}
